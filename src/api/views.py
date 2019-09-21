f62b25be64490a8aa3f6a75896b2fcc2d5633903
LeicaSimile <leicasimile@gmail.com>
2019-09-21 17:08:23 -0400

    Add api decorators

--- src/api/views.py
+++ src/api/views.py
@@ -1,18 +1,25 @@
 from django.shortcuts import render
 import lyricsgenius
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(["GET"])
 def artist(request):
-    return render(request, '', {})
+    return
 
+@api_view(["GET"])
 def song(request):
     return
 
+@api_view(["GET"])
 def songs_by_artist(request):
     return
 
+@api_view(["GET"])
 def lyrics(request):
     return
 
+@api_view(["GET"])
 def lyrics_by_artist(request):
     return
 
