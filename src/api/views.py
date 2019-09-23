8ce4ab2c86b573b55ce75b67d4961b13b109f009
Angelica Catalan <leicasimile@gmail.com>
2019-09-23 19:26:09 -0400

    Return artist and song

--- src/api/views.py
+++ src/api/views.py
@@ -6,11 +6,13 @@ from .constants import GENIUS_ACCESS_TOKEN
 
 @api_view(["GET"])
 def artist(request):
-    return
+    genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
+    return Response(data=genius.get_artist(request.GET.get("id")))
 
 @api_view(["GET"])
 def song(request):
-    return
+    genius = lyricsgenius.Genius(GENIUS_ACCESS_TOKEN)
+    return Response(data=genius.get_song(request.GET.get("id")))
 
 @api_view(["GET"])
 def songs_by_artist(request):
@@ -19,10 +21,5 @@ def songs_by_artist(request):
     return Response(data=[song.to_dict() for song in artist.songs])
 
 @api_view(["GET"])
-def lyrics(request):
+def generated_song(request):
     return
-
-@api_view(["GET"])
-def lyrics_by_artist(request):
-    return
-
