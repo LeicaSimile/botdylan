5560c651bba5d0894f18dacda43f2434ca7497e1
LeicaSimile <leicasimile@gmail.com>
2019-10-16 19:44:31 -0400

    Add input field to react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form action="http://127.0.0.1:8000/api/v1/songs" method="GET">
+          <input id="artist" name="artist" type="text" />
+          <input type="submit" value="Write Song" />
+        </form>
       </header>
     </div>
   );
