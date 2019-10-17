a9e0b0a4bb486d4a33f856b624153c52674fbbcd
LeicaSimile <leicasimile@gmail.com>
2019-10-16 21:31:49 -0400

    Use Bob Dylan as placeholder text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,10 @@ function App() {
       <header className="App-header">
         <form action="http://127.0.0.1:8000/api/v1/songs" method="GET">
           <div className="field">
-            <label className="label has-text-left has-text-grey-light is-uppercase">Artist Name</label>
+            <label className="label has-text-left has-text-light is-uppercase">Artist Name</label>
             <div className="field has-addons">
               <div className="control">
-                <input placeholder="e.g. Taylor Swift" id="artist" name="artist" type="text" className="input is-large" />
+                <input placeholder="e.g. Bob Dylan" id="artist" name="artist" type="text" className="input is-large" />
               </div>
               <div className="control">
                 <input type="submit" value="Write Song" className="button is-large is-primary" />
