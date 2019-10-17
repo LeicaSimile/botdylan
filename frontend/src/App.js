c80771b948386d25958ef2f9f9c43230ddef5702
LeicaSimile <leicasimile@gmail.com>
2019-10-16 20:50:59 -0400

    Add bulma

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <form action="http://127.0.0.1:8000/api/v1/songs" method="GET">
-          <input id="artist" name="artist" type="text" />
-          <input type="submit" value="Write Song" />
+          <div className="field">
+            <label className="label has-text-left has-text-grey-light is-uppercase">Artist Name</label>
+            <div className="field has-addons">
+              <div className="control">
+                <input placeholder="e.g. Taylor Swift" id="artist" name="artist" type="text" className="input is-large" />
+              </div>
+              <div className="control">
+                <input type="submit" value="Write Song" className="button is-large is-primary" />
+              </div>
+            </div>
+          </div>
         </form>
       </header>
     </div>
